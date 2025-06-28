479034121ed31ba555b5e7a202797b064e098f69
alexhetley6107 <hetleyofficial@gmail.com>
2025-06-28 15:08:11 +0300

    feat: docker file

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { TaskModule } from './task/task.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ConfigModule.forRoot(), AuthModule, TaskModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/pyd'),
+    ConfigModule.forRoot(),
+    AuthModule,
+    TaskModule,
+  ],
   controllers: [],
   providers: [],
 })
